42063ad55ba5cbb1b2692696c45ee6bc79f09e73
Psukhai <44929863+Psukhai@users.noreply.github.com>
2020-12-16 16:00:46 +0800

    Update Pixiv_Premium.js

--- Pixiv_Premium.js
+++ Pixiv_Premium.js
@@ -1,4 +1,4 @@
 let body = JSON.parse($response.body)
-body['response']['user']['is_premium']['x_restrict'] = true
+body['response']['user']['is_premium'] = true
 body = JSON.stringify(body)
 $done({body})
