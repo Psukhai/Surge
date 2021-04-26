34023d4016231c63ae78187eb62bdd27aaa7b4e5
Psukhai <44929863+Psukhai@users.noreply.github.com>
2021-04-27 07:10:45 +0800

    Update Sub.js

--- Sub.js
+++ Sub.js
@@ -102,7 +102,7 @@ function formatTime( time ) {
     let year = dateObj.getFullYear();
     let month = dateObj.getMonth() + 1;
     let day = dateObj.getDate();
-    return year + "年" + month + "月" + day + "日";
+    return year +"-"+ month +"-" + day;
 }
 
 function sendNotification(usageRate, expire, infoList) {
